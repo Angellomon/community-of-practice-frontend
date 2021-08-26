ab7047dc5c6e9ca209d35d87734390e85e76dcf2
Angellomon <angelmtzdiaz@gmail.com>
2021-08-25 19:57:00 -0500

    feat(pieza): implementación de placeholder invisible para establecer animación fade

--- src/lib/components/rompecabezas/PiezaAzul.svelte
+++ src/lib/components/rompecabezas/PiezaAzul.svelte
@@ -1,24 +1,39 @@
 <script>
+  import { fade } from 'svelte/transition';
   import { PIEZA_LENGTH } from '../../util';
 
   export let src = '';
   export let scale = 1;
   export let top = 0;
   export let left = 0;
+
+  export let hidden = true;
 </script>
 
-<img
-  {src}
-  alt="pieza"
-  width={PIEZA_LENGTH * scale}
-  height={PIEZA_LENGTH * scale}
-  style={`margin-top: ${top}px; margin-left: ${left}px`}
-/>
+<span
+  style={`width: ${PIEZA_LENGTH * scale}px; height: ${
+    PIEZA_LENGTH * scale
+  }px; margin-top: ${top}px; margin-left: ${left}px;`}
+>
+  {#if !hidden}
+    <img
+      {src}
+      transition:fade
+      alt=""
+      width={PIEZA_LENGTH * scale}
+      height={PIEZA_LENGTH * scale}
+      class:hidden
+    />
+  {/if}
+</span>
 
 <style>
   img {
     padding: 0;
     /* width: 100%;
     height: 100%; */
+  }
+  .hidden {
+    visibility: hidden;
   }
 </style>
