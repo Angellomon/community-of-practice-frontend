ff4ec5ddf0c2ed97efad6c1960ed95fd8bd122ac
Angellomon <angelmtzdiaz@gmail.com>
2021-08-25 19:55:07 -0500

    feat(animacion): implementación de timeout para settear enimacion

--- src/lib/components/rompecabezas/Rompecabezas.svelte
+++ src/lib/components/rompecabezas/Rompecabezas.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import PiezaPrincipal from './PiezaPrincipal.svelte';
   import PiezaAzul from './PiezaAzul.svelte';
 
@@ -10,29 +11,83 @@
   import srcPieza7 from '../../../assets/piezas/03.png';
   import srcPieza8 from '../../../assets/piezas/02.png';
   import srcPieza9 from '../../../assets/piezas/01.png';
+  import { onDestroy } from 'svelte';
 
-  const SCALE = 0.65;
+  const dispatch = createEventDispatcher();
+
+  let pieza1Hidden = true;
+  let pieza2Hidden = true;
+  let pieza3Hidden = true;
+  let pieza4Hidden = true;
+  let pieza6Hidden = true;
+  let pieza7Hidden = true;
+  let pieza8Hidden = true;
+  let pieza9Hidden = true;
+
+  let timeout1;
+  let timeout2;
+  let timeout3;
+  let timeout4;
+  let timeout6;
+  let timeout7;
+  let timeout8;
+  let timeout9;
 
   const handleClick = () => {
-    console.log('click');
+    timeout1 = setTimeout(() => {
+      pieza1Hidden = false;
+    }, 750);
+    timeout2 = setTimeout(() => {
+      pieza2Hidden = false;
+    }, 350);
+    timeout3 = setTimeout(() => {
+      pieza3Hidden = false;
+    }, 850);
+    timeout4 = setTimeout(() => {
+      pieza4Hidden = false;
+    }, 250);
+    timeout6 = setTimeout(() => {
+      pieza6Hidden = false;
+    }, 150);
+    timeout7 = setTimeout(() => {
+      pieza7Hidden = false;
+    }, 550);
+    timeout8 = setTimeout(() => {
+      pieza8Hidden = false;
+    }, 450);
+    timeout9 = setTimeout(() => {
+      pieza9Hidden = false;
+    }, 650);
+    dispatch('click');
   };
+
+  onDestroy(() => {
+    clearTimeout(timeout1);
+    clearTimeout(timeout2);
+    clearTimeout(timeout3);
+    clearTimeout(timeout4);
+    clearTimeout(timeout6);
+    clearTimeout(timeout7);
+    clearTimeout(timeout8);
+    clearTimeout(timeout9);
+  });
 </script>
 
 <section>
   <div class="row">
-    <PiezaAzul src={srcPieza1} scale={1} top={0} left={0} />
-    <PiezaAzul src={srcPieza2} scale={1} top={-15} left={-36} />
-    <PiezaAzul src={srcPieza3} scale={1} top={-18} left={-45} />
+    <PiezaAzul src={srcPieza1} scale={1} top={0} left={0} hidden={pieza1Hidden} />
+    <PiezaAzul src={srcPieza2} scale={1} top={-15} left={-36} hidden={pieza2Hidden} />
+    <PiezaAzul src={srcPieza3} scale={1} top={-18} left={-45} hidden={pieza3Hidden} />
   </div>
   <div class="row">
-    <PiezaAzul src={srcPieza4} scale={1.0} top={-39} left={3} />
+    <PiezaAzul src={srcPieza4} scale={1.0} top={-39} left={3} hidden={pieza4Hidden} />
     <PiezaPrincipal on:click={handleClick} scale={1} top={-56} left={-40} />
-    <PiezaAzul src={srcPieza6} scale={1} top={-41} left={-37} />
+    <PiezaAzul src={srcPieza6} scale={1} top={-41} left={-37} hidden={pieza6Hidden} />
   </div>
   <div class="row">
-    <PiezaAzul src={srcPieza7} scale={1} top={-39} left={16} />
-    <PiezaAzul src={srcPieza8} scale={1} top={-53.5} left={-37} />
-    <PiezaAzul src={srcPieza9} scale={1} top={-51.5} left={-38} />
+    <PiezaAzul src={srcPieza7} scale={1} top={-39} left={16} hidden={pieza7Hidden} />
+    <PiezaAzul src={srcPieza8} scale={1} top={-53.5} left={-37} hidden={pieza8Hidden} />
+    <PiezaAzul src={srcPieza9} scale={1} top={-51.5} left={-38} hidden={pieza9Hidden} />
   </div>
 </section>
 
