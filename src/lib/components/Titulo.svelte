e488550f6920a555090e7b27a3df10fdd4fb67c4
Angellomon <angelmtzdiaz@gmail.com>
2021-09-02 12:19:16 -0500

    fix(titulo): corrección de espacio entre lines

--- src/lib/components/Titulo.svelte
+++ src/lib/components/Titulo.svelte
@@ -1,15 +1,15 @@
 <script>
-  import Emphasis from "./Emphasis.svelte";
+  import Emphasis from './Emphasis.svelte';
 
   export let on = false;
 </script>
 
-<h2 class:on>community <br /><Emphasis>of</Emphasis> practice</h2>
+<h2 class:on><Emphasis>community</Emphasis><br />of practice</h2>
 
 <style>
   h2 {
     color: white;
-    font-size: 3em;
+    font-size: 2.9em;
     text-align: right;
     line-height: 1em;
     /* margin-top: -100px; */
